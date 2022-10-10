94c9c393fd3fd596b33099a2fdc43c9bf7336986
Timi-imo2 <timiimoukhuede6@gmail.com>
2022-10-10 12:16:55 -0400

    Done

---
+++ app.js
@@ -0,0 +1,42 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`Jan 1, ${currentYear + 1} 00:00:00`);
+
+//Set background year
+year.innerText = currentYear + 1;
+
+//Update cd time
+function UpdateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    console.log(d);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    //add values to dom
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+
+}
+
+
+//Show spinner before cd
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+//Run this every second
+setInterval(UpdateCountdown, 1000);
